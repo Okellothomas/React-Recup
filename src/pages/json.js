71ab9a2583dc0f686bd6ff0d146018a4ec5c6a68
Okellothomas <thomasolal33@gmail.com>
2023-03-10 14:02:49 +0300

    the json

---
+++ src/pages/json.js
@@ -0,0 +1,79 @@
+export default [
+    {
+        name: "one",
+        desc: "it is the number one",
+        number: 1,
+        school: [2,4,5]
+        
+    },
+    {
+        name: "two",
+        desc: "it is the number two",
+        number: 2,
+        school: [2,4,5]
+        
+    },
+    {
+        name: "three",
+        desc: "it is the number three",
+        number: 3,
+        school: [2,4,5]
+        
+    },
+    {
+        name: "four",
+        desc: "it is the number four",
+        number: 4,
+        school: [2,4,5]
+        
+    },
+    {
+        name: "one",
+        desc: "it is the number one",
+        number: 1,
+        school: [2,4,5]
+        
+    },
+    {
+        name: "five",
+        desc: "it is the number five",
+        number: 5,
+        school: [2,4,5]
+        
+    },
+    {
+        name: "six",
+        desc: "it is the number six",
+        number: 6,
+        school: [2,4,5]
+        
+    },
+    {
+        name: "seven",
+        desc: "it is the number seven",
+        number: 7,
+        school: [2,4,5]
+        
+    },
+    {
+        name: "eight",
+        desc: "it is the number eight",
+        number: 8,
+        school: [2,4,5]
+        
+    },
+    {
+        name: "nine",
+        desc: "it is the number nine",
+        number: 9,
+        school: [2,4,5]
+        
+    },
+    {
+        name: "ten",
+        desc: "it is the number ten",
+        number: 10,
+        school: [2,4,5]
+        
+    }
+]
